6aaec2f330b9d71048fa4cf48594472327565f8c
GermanoC <105460981+GermanoCesarSS@users.noreply.github.com>
2024-11-22 16:40:41 -0300

    Implementando Rotas Crud

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,67 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-@app.get('/')
-def test_read_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)  # Arrange (organização)
-
+def test_read_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')  # Act (ação)
 
     assert response.status_code == HTTPStatus.OK  # asseert
-    assert response.json() == {'message': 'Olar mundo!!'}
+    assert response.json() == {'message': 'Olá Mundo!!!'}
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'testusername',
+            'password': 'password',
+            'email': 'test@test.com',
+        },
+    )
+
+    # VAlidar o status code correto?
+    assert response.status_code == HTTPStatus.CREATED
+    # Validar UserPublic
+    assert response.json() == {
+        'id': 1,
+        'username': 'testusername',
+        'email': 'test@test.com',
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'id': 1,
+                'username': 'testusername',
+                'email': 'test@test.com',
+            }
+        ]
+    }
+
+
+def test_updatee_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'id': 1,
+            'username': 'testusername2',
+            'email': 'test@test.com',
+            'password': '123',
+        },
+    )
+
+    assert response.json() == {
+        'id': 1,
+        'username': 'testusername2',
+        'email': 'test@test.com',
+    }
+
+
+def teste_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.json() == {'message': 'User deleted'}
