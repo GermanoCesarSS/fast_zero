b6d8ab1ab30c0131abf67bda40e07d40e33f99c6
GermanoC <105460981+GermanoCesarSS@users.noreply.github.com>
2024-11-22 17:32:37 -0300

    Meus exercicios 2

--- tests/test_app.py
+++ tests/test_app.py
@@ -28,6 +28,17 @@ def test_create_user(client):
     }
 
 
+def test_read_users__exercicio(client):
+    response = client.get('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'username': 'testusername',
+        'email': 'test@test.com',
+    }
+
+
 def test_read_users(client):
     response = client.get('/users/')
 
@@ -43,12 +54,12 @@ def test_read_users(client):
     }
 
 
-def test_updatee_user(client):
+def test_update_user(client):
     response = client.put(
         '/users/1',
         json={
             'id': 1,
-            'username': 'testusername2',
+            'username': 'testusername1',
             'email': 'test@test.com',
             'password': '123',
         },
@@ -56,12 +67,38 @@ def test_updatee_user(client):
 
     assert response.json() == {
         'id': 1,
-        'username': 'testusername2',
+        'username': 'testusername1',
         'email': 'test@test.com',
     }
 
 
-def teste_delete_user(client):
+def test_update_user_not_found__exercicio(client):
+    response = client.put(
+        '/users/10',
+        json={
+            'id': 10,
+            'username': 'testusername10',
+            'email': 'test@test.com',
+            'password': '123',
+        },
+    )
+    assert response.status_code == HTTPStatus.NOT_FOUND
+
+
+def test_delete_user(client):
     response = client.delete('/users/1')
 
     assert response.json() == {'message': 'User deleted'}
+
+
+def test_delete_user_not_found__exercicio(client):
+    response = client.delete('/users/10')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+
+
+def test_get_user_not_found__exercicio(client):
+    response = client.get('users/666')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
