29dab5247a46bb991fb974c7d2cd8dd19e24cb94
GermanoC <105460981+GermanoCesarSS@users.noreply.github.com>
2024-11-28 22:05:13 -0300

    DOCKER NAO BUILDA

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@
 poetry run alembic upgrade head
 
 # Inicia a aplicação
-poetry run uvicorn fast_zero/app.py --host 0.0.0.0 --port 8000 --proxy-headers
+poetry run fastapi run fast_zero/app.py --host 0.0.0.0 --port 8000
--- fast_zero/app.py
+++ fast_zero/app.py
@@ -7,6 +7,7 @@ from fast_zero.schemas import Message
 
 app = FastAPI()
 
+
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(todo.router)
